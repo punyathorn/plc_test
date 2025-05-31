6b5479eeb93b858877702349414d4924e67982e3
Punyathorn <93460088+punyathorn@users.noreply.github.com>
2025-05-31 08:03:01 +0700

    Add files via upload

--- gui2.py
+++ gui2.py
@@ -33,10 +33,11 @@ SD5544 = IntVar()
 SD5542 = IntVar()
 SD5504 = IntVar()
 SD5502 = IntVar()
-trig = IntVar()
 S = IntVar()
+DD0 = IntVar()
+DD2 = IntVar()
+DD4 = IntVar()
 S.set(0)
-trig.set(0)
 write_queue = Queue()
 response_queue = Queue()
 stop_threads = Event()
@@ -55,9 +56,6 @@ def con_thread():
     except Exception as e:
         status.set("Status: Connection Failed")
         messagebox.showerror(title="Connection Error", message=str(e))
-
-def dosthcv():
-    print('Hallo')
 
 def read_thread():
     while not stop_threads.is_set():
@@ -80,21 +78,21 @@ def read_thread():
                 client.batchread_wordunits(headdevice="SD5584", readsize=1)
             ]
 
-            M150 = client.batchread_bitunits(headdevice="M149", readsize=1)
-            if M150[0] == 1 and trig.get() == 0:
-                print('ACTIVATED')
-                print(trig.get())
-                trig.set(1)
-                dosthcv()
-            if M150[0] == 0:
-                trig.set(0)
+            DD = [
+                client.batchread_wordunits(headdevice="D0", readsize=1),
+                client.batchread_wordunits(headdevice="D2", readsize=1),
+                client.batchread_wordunits(headdevice="D4", readsize=1)
+            ]
+
             SD5582.set(SD[2][0])
             SD5542.set(SD[1][0])
             SD5502.set(SD[0][0])
             SD5504.set(SPSD[0][0])
             SD5544.set(SPSD[1][0])
             SD5584.set(SPSD[2][0])
-
+            DD0.set(DD[0])
+            DD2.set(DD[1])
+            DD4.set(DD[2])
             if not write_queue.empty():
                 data = write_queue.get()
                 try:
@@ -112,7 +110,7 @@ def read_thread():
 
         except Exception as e:
             print(f"[Read Thread Error] {e}")
-        time.sleep(0.1)
+        time.sleep(0.2)
 
 def connect():
     global t, t2
@@ -123,7 +121,7 @@ def connect():
     t2.start()
 
 def one_axis_arm_throw():
-    q = messagebox.askyesnocancel(title="1 Axis Arm Throw", message=f" Arm Speed d4: {speed_a1.get()} \n Arm Angle d2: {angle_a1.get()} \n Plate Angle d0: 0 \n Do you want to continue?")
+    q = messagebox.askyesnocancel(title="1 Axis Arm Throw", message=f" Arm Speed d2: {speed_a1.get()} \n Arm Angle d4: {angle_a1.get()} \n Plate Angle d0: 0 \n Do you want to continue?")
     if not q:
         return
     write_queue.put([0, angle_a1.get(), speed_a1.get()])
@@ -134,7 +132,7 @@ def one_axis_arm_throw():
         messagebox.showerror("Timeout", "No response from write thread")
 
 def full_throw():
-    q = messagebox.askyesnocancel(title="Full Throw", message=f" Arm Speed d4: {speed_a1.get()} \n Arm Angle d2: {angle_a1.get()} \n Plate Angle d0: {angle_a2.get()} \n Do you want to continue?")
+    q = messagebox.askyesnocancel(title="Full Throw", message=f" Arm Speed d2: {speed_a1.get()} \n Arm Angle d4: {angle_a1.get()} \n Plate Angle d0: {angle_a2.get()} \n Do you want to continue?")
     if not q:
         return
     write_queue.put([angle_a2.get(), angle_a1.get(), speed_a1.get()])
@@ -156,19 +154,19 @@ sidebar_frame.grid(row=0, column=0, rowspan=2, sticky="nsew")
 Content_Label = customtkinter.CTkLabel(sidebar_frame, text="Arm Speed (pulse):", font=Medium)
 Content_Label.grid(row=0, column=0, padx=10, pady=25)
 
-Content_Entry = customtkinter.CTkEntry(sidebar_frame, textvariable=speed_a1, font=Medium, width=100)
+Content_Entry = customtkinter.CTkEntry(sidebar_frame, textvariable=speed_a1, font=Medium, width=100, state='disabled')
 Content_Entry.grid(row=1, column=0, padx=10, pady=25)
 
 Content_Label = customtkinter.CTkLabel(sidebar_frame, text="Arm Angle (pulse):", font=Medium)
 Content_Label.grid(row=2, column=0, padx=10, pady=25)
 
-Content_Entry = customtkinter.CTkEntry(sidebar_frame, textvariable=angle_a1, font=Medium, width=100)
+Content_Entry = customtkinter.CTkEntry(sidebar_frame, textvariable=angle_a1, font=Medium, width=100, state='disabled')
 Content_Entry.grid(row=3, column=0, padx=10, pady=25)
 
 Content_Label = customtkinter.CTkLabel(sidebar_frame, text="Plate Angle (pulse):", font=Medium)
 Content_Label.grid(row=4, column=0, padx=10, pady=25)
 
-Content_Entry = customtkinter.CTkEntry(sidebar_frame, textvariable=angle_a2, font=Medium, width=100)
+Content_Entry = customtkinter.CTkEntry(sidebar_frame, textvariable=angle_a2, font=Medium, width=100, state='disabled')
 Content_Entry.grid(row=5, column=0, padx=10, pady=25)
 
 Content_Label = customtkinter.CTkLabel(sidebar_frame, text="Plate Angle (read) (pulse):", font=Medium)
@@ -183,6 +181,12 @@ Content_Label.grid(row=2, column=1, padx=10, pady=25)
 Content_Entry = customtkinter.CTkEntry(sidebar_frame, textvariable=SD5584, font=Medium, width=100, state='disabled')
 Content_Entry.grid(row=3, column=1, padx=10, pady=25)
 
+Content_Label = customtkinter.CTkLabel(sidebar_frame, text="D0:", font=Medium)
+Content_Label.grid(row=4, column=1, padx=10, pady=25)
+
+Content_Entry = customtkinter.CTkEntry(sidebar_frame, textvariable=DD0, font=Medium, width=100, state='disabled')
+Content_Entry.grid(row=5, column=1, padx=10, pady=25)
+
 Content_Label = customtkinter.CTkLabel(sidebar_frame, text="Arm Angle (read) (pulse):", font=Medium)
 Content_Label.grid(row=0, column=2, padx=10, pady=25)
 
@@ -195,6 +199,12 @@ Content_Label.grid(row=2, column=2, padx=10, pady=25)
 Content_Entry = customtkinter.CTkEntry(sidebar_frame, textvariable=SD5544, font=Medium, width=50, state='disabled')
 Content_Entry.grid(row=3, column=2, padx=10, pady=25)
 
+Content_Label = customtkinter.CTkLabel(sidebar_frame, text="D2:", font=Medium)
+Content_Label.grid(row=4, column=2, padx=10, pady=25)
+
+Content_Entry = customtkinter.CTkEntry(sidebar_frame, textvariable=DD2, font=Medium, width=100, state='disabled')
+Content_Entry.grid(row=5, column=2, padx=10, pady=25)
+
 Content_Label = customtkinter.CTkLabel(sidebar_frame, textvariable=status, font=Medium)
 Content_Label.grid(row=0, column=3, padx=10, pady=25)
 
@@ -206,5 +216,11 @@ sidebar_button_1.grid(row=2, column=3, padx=10, pady=25)
 
 sidebar_button_2 = customtkinter.CTkButton(sidebar_frame, text="Full Throw", command=full_throw)
 sidebar_button_2.grid(row=3, column=3, padx=10, pady=25)
+
+Content_Label = customtkinter.CTkLabel(sidebar_frame, text="D4:", font=Medium)
+Content_Label.grid(row=4, column=3, padx=10, pady=25)
+
+Content_Entry = customtkinter.CTkEntry(sidebar_frame, textvariable=DD4, font=Medium, width=100, state='disabled')
+Content_Entry.grid(row=5, column=3, padx=10, pady=25)
 
 GUI.mainloop()
